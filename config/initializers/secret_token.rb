d69438063d75bcad84356cb369905fddd5eb527f
Saran Yamasathien <pom.saran@gmail.com>
2011-12-04 10:23:46 +0700

    create website template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Siamcomplain::Application.config.secret_token = '2dc252553cb18d7e1972cddc40f4ec9a52652e006cf03c9f0e8d9add72b200bb29003f8731bb1eb2916d041c216cd83077e8e0c7cc83a3be4e53dbc583a71993'
