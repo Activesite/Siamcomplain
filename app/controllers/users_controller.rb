82647abc952f80ec19add0f26bac545912897822
Saran Yamasathien <pom.saran@gmail.com>
2012-02-18 23:38:49 +0700

    Sign up Page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,11 @@
 class UsersController < ApplicationController
-  def new
-  end
 
   def show
       @user = User.find(params[:id])
       @title = @user.name
   end
   def new
+      @user = User.new
       @title = "Sign up"
   end
 
