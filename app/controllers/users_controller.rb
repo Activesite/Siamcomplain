9a4af55532fd174ea366d6103e4777343614f272
Saran Yamasathien <pom.saran@gmail.com>
2012-02-19 22:40:17 +0700

    sign up with redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
       @user = User.new(params[:user])
       if @user.save
+          redirect_to @user
       else
           @title = "Sign up"
           render 'new'
